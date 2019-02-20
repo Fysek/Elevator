412ef227fdbd5fc8f8dfee3476a8ca5e83208712
Mateusz Dyrdół <mateuszdyrdol@gmail.com>
2019-02-20 21:44:06 +0100

    working module and testbench

--- elevator.v
+++ elevator.v
@@ -8,24 +8,29 @@ parameter BUTTONS_WIDTH = 6
 (
 	input clk,
 	input reset,
-	input service,
-	input weight_check,
-	input fan,
+	//input service,
+	//input fan,
+	input open_btn,
+	input close_btn,
 	input [BUTTONS_WIDTH-1:0] btn_num_in, // wewnatrz windy
 	input [BUTTONS_WIDTH-1:0] btn_up_out, //na zewnatrz do gory
 	input [BUTTONS_WIDTH-1:0] btn_down_out,//na zewnatrz na dół
-	output reg [1:0] engine_up,
-	output reg [1:0] engine_down,
+	output reg engine_up,
+	output reg engine_down,
+	output reg open_door,
+	output reg close_door,
 	output reg [BUTTONS_WIDTH-1:0] level_display
 );
     
 	reg [BUTTONS_WIDTH-1:0] current_level;
 	reg [BUTTONS_WIDTH-1:0] active_levels;// te które są wcisnete
+	reg reached,overload=0;
+	
 	
 	reg[2:0] state, next_state;
 	parameter IDLE=3'b000, OPEN=3'b001, CLOSE=3'b010, UP=3'b011,DOWN=3'b100,WAIT=3'b101;
 	
-	reg[2:0] floor, ;
+	reg[2:0] floor, next_floor;
 	parameter GROUND=3'b000,FLOOR1=3'b001,FLOOR2=3'b010,FLOOR3=3'b011,FLOOR4=3'b100,FLOOR5=3'b101,FLOOR6=3'b110,FLOOR7=3'b111;
 	
 	
@@ -48,7 +53,7 @@ parameter BUTTONS_WIDTH = 6
 	
 		IDLE:
 		begin
-			if(inopen)
+			if(open_btn)
 				next_state=OPEN;
 			else if(floor<next_floor)
 				next_state=UP;
@@ -57,7 +62,6 @@ parameter BUTTONS_WIDTH = 6
 			else 
 				if(floor==next_floor)
 				begin
-					if(inG|in1|in2|in3|in4|in5|in6|in7)
 						next_state=OPEN;
 				end
 				else 
@@ -66,7 +70,7 @@ parameter BUTTONS_WIDTH = 6
 
 		OPEN:
 		begin
-			if(inclose)
+			if(close_btn)
 				next_state=CLOSE;
 			else 
 				next_state=WAIT;
@@ -74,7 +78,7 @@ parameter BUTTONS_WIDTH = 6
 	
 		CLOSE:
 		begin
-			if(inopen)
+			if(open_btn)
 				next_state=OPEN;
 			else 
 				next_state=IDLE;
@@ -112,13 +116,7 @@ parameter BUTTONS_WIDTH = 6
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
+
 	/*
     always @(posedge clk or reset)
         if(reset)
@@ -128,10 +126,8 @@ parameter BUTTONS_WIDTH = 6
             end
 		else
 			begin	
-			
-			
-			
-			
+				
 			
 			end*/
-endmodule //elevator
+endmodule 
+//elevator
--- elevator_tb.v
+++ elevator_tb.v
@@ -1,8 +1,49 @@
 //elevator
+`include "elevator.v"
 
-module elevator_tb();
-reg clk,reset,inG,in1,in2,in3,in4,in5,in6,in7,inopen,inclose;
-wire open,close,up,down;
+module elevator_tb;
+ //inputs
+ parameter BUTTONS_WIDTH = 6;
+ reg clk;
+ reg reset;
+ reg open_btn;
+ reg close_btn;
+ reg [BUTTONS_WIDTH-1:0] btn_num_in; // wewnatrz windy
+ reg [BUTTONS_WIDTH-1:0] btn_up_out; //na zewnatrz do gory
+ reg [BUTTONS_WIDTH-1:0] btn_down_out;
+ //outputs
+ wire engine_up;
+ wire engine_down;
+ wire open_door;
+ wire close_door;
+ wire [BUTTONS_WIDTH-1:0] level_display;
+ 
+ 
+ 
+elevator elevator_inst(
+		.clk(clk),
+		.reset(reset),
+		.open_btn(open_btn),
+		.close_btn(close_btn),
+		.btn_num_in(btn_num_in),
+		.btn_up_out(btn_up_out),
+		.btn_down_out(btn_down_out),
+		.engine_up(engine_up),
+		.engine_down(engine_down),
+		.open_door(open_door),
+		.close_door(close_door),
+		.level_display(level_display)
+		);
 
-reg[7:0] ins;
-elevator DUT(clk,rst,inG,in1,in2,in3,in4,in5,in6,in7,inopen,inclose,open,close,up,down);
+
+initial
+	begin
+		$dumpfile("elevator_tb.vcd");
+	  	$dumpvars(0,clk,reset,open_btn,close_btn,btn_num_in,btn_up_out,btn_down_out,engine_up,engine_down,open_door,level_display);
+		$monitor($time,": clk=%b reset=%b open_btn=%b close_btn=%b btn_num_in=%d btn_up_out=%d btn_down_out=%d | engine_up=%b engine_down=%b open_door=%b close_door=%b level_display=%b"
+		,clk,reset,open_btn,close_btn,btn_num_in,btn_up_out,btn_down_out,engine_up,engine_down,open_door,close_door,level_display);
+
+	#10 $finish;
+	end
+
+endmodule
