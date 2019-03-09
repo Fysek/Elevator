0acb860ff8f361d6c7b5fc84b210b1d39499b6ce
Mateusz Dyrdół <mateuszdyrdol@gmail.com>
2019-03-09 15:16:39 +0100

    update on buttons

--- buttons.v
+++ buttons.v
@@ -7,51 +7,49 @@ parameter BUTTONS_WIDTH = 8
 (
 	input clk,
 	input reset,
-	input [BUTTONS_WIDTH-1:0] btn_num_in, // wewnatrz windy
+	input [BUTTONS_WIDTH-1:0] btn_in, // wewnatrz windy
 	input [BUTTONS_WIDTH-1:0] btn_up_out, //na zewnatrz do gory
 	input [BUTTONS_WIDTH-1:0] btn_down_out,//na zewnatrz na dół
+	input [BUTTONS_WIDTH-1:0] inactive_in_levels, // wewnatrz windy
+	input [BUTTONS_WIDTH-1:0] inactive_out_up_levels, //na zewnatrz do gory
+	input [BUTTONS_WIDTH-1:0] inactive_out_down_levels,//na zewnatrz na dół
 	output reg [BUTTONS_WIDTH-1:0] active_in_levels,
 	output reg [BUTTONS_WIDTH-1:0] active_out_up_levels,
 	output reg [BUTTONS_WIDTH-1:0] active_out_down_levels
 );
 
-	reg [BUTTONS_WIDTH-1:0] reg_active_in_levels;// te które są wcisnete wewnatrz
-	reg [BUTTONS_WIDTH-1:0] reg_active_out_up_levels;// te które są wcisnete na zew
-	reg [BUTTONS_WIDTH-1:0] reg_active_out_down_levels;// te które są wcisnete na zew
-
+	integer index;
 	
-	always @(*) 
+	always @(posedge clk or negedge reset) 
 	begin
 		if(!reset) begin
-			reg_active_in_levels=0;
-			reg_active_out_up_levels=0;
-			reg_active_out_down_levels=0;
+			active_in_levels<=0;
+			active_out_up_levels<=0;
+			active_out_down_levels<=0;
 		end
-	
-	    for(index=0; index<BUTTONS_WIDTH; index=index+1) begin
-			if(btn_num_in[index] == 1) begin
-				reg_active_in_levels[index] = 1;
-			end
+		else begin
+			for(index=0; index<BUTTONS_WIDTH; index=index+1) begin
+				//in
+				if(btn_in[index] == 1) begin
+					active_in_levels[index] <= 1;
+				end
+				else if (inactive_in_levels[index] == 1) begin
+					active_in_levels[index] <= 0;
+				end
+				//up
+				if(btn_up_out[index] == 1) begin
+					active_out_up_levels[index] <= 1;
+				end else if (inactive_out_up_levels[index] == 1) begin
+					active_out_up_levels[index] <= 0;
+				end
+				//down
+				if(btn_down_out[index] == 1) begin
+					active_out_down_levels[index] <= 1;
+				end else if (inactive_out_down_levels[index] == 1) begin
+					active_out_down_levels[index] <= 0;
+				end
+			end				
 		end
-		
-		for(index=0; index<BUTTONS_WIDTH; index=index+1) begin
-			if(btn_up_out[index] == 1) begin
-				reg_active_out_up_levels[index] = 1;
-			end
-		end
-		
-		for(index=0; index<BUTTONS_WIDTH; index=index+1) begin
-			if(btn_down_out[index] == 1) begin
-				reg_active_out_down_levels[index] = 1;
-			end
-		end
-		
-		active_in_levels = reg_active_in_levels;
-		active_out_up_levels = reg_active_out_up_levels;
-		active_out_down_levels = reg_active_out_down_levels;
-		
-		
 	end	
-
 endmodule 
 //buttons
--- buttons_tb.v
+++ buttons_tb.v
@@ -5,9 +5,12 @@ module buttons_tb;
 	parameter BUTTONS_WIDTH = 8;
 	reg clk;
 	reg reset;
-	reg [BUTTONS_WIDTH-1:0] btn_num_in; 
+	reg [BUTTONS_WIDTH-1:0] btn_in; 
 	reg [BUTTONS_WIDTH-1:0] btn_up_out; 
 	reg [BUTTONS_WIDTH-1:0] btn_down_out;
+	reg [BUTTONS_WIDTH-1:0] inactive_in_levels;
+	reg [BUTTONS_WIDTH-1:0] inactive_out_up_levels;
+	reg [BUTTONS_WIDTH-1:0] inactive_out_down_levels;
 	wire [BUTTONS_WIDTH-1:0] active_in_levels;
 	wire [BUTTONS_WIDTH-1:0] active_out_up_levels;
 	wire [BUTTONS_WIDTH-1:0] active_out_down_levels;
@@ -15,9 +18,12 @@ module buttons_tb;
  buttons buttons_inst(
 		.clk(clk),
 		.reset(reset),
-		.btn_num_in(btn_num_in),
+		.btn_in(btn_in),
 		.btn_up_out(btn_up_out),
 		.btn_down_out(btn_down_out),
+		.inactive_in_levels(inactive_in_levels),
+		.inactive_out_up_levels(inactive_out_up_levels),
+		.inactive_out_down_levels(inactive_out_down_levels),
 		.active_in_levels(active_in_levels),
 		.active_out_up_levels(active_out_up_levels),
 		.active_out_down_levels(active_out_down_levels)
@@ -31,26 +37,31 @@ always
 initial
 	begin
 	$dumpfile("buttons_tb.vcd");
-	$dumpvars(0,clk,reset,btn_num_in,btn_up_out,btn_down_out,active_in_levels,active_out_up_levels,active_out_down_levels);
+	$dumpvars(0,clk,reset,btn_in,btn_up_out,btn_down_out,inactive_in_levels,inactive_out_up_levels,inactive_out_down_levels,active_in_levels,active_out_up_levels,active_out_down_levels);
 	clk = 1'b0;
-	btn_num_in = 0;
+	btn_in = 0;
 	btn_up_out = 0;
 	btn_down_out = 0;
+	inactive_in_levels = 0;
+	inactive_out_up_levels = 0;
+	inactive_out_down_levels = 0;
 	#5 reset = 0;
 	#5 reset = 1;
 	#20 btn_up_out[4] = 1;	
 	#10 btn_up_out[5] = 1;
-	#10 btn_num_in[1] = 1;
+	#10 btn_in[1] = 1;
 	#10 btn_down_out[7] = 1;
-	#10 btn_num_in[3] = 0;
+	#10 btn_in[3] = 0;
 	#10 btn_down_out[7] = 0;
 		btn_up_out[5] = 0;
 	#10 btn_up_out[4] = 0;
 		btn_down_out[7] = 1;
-		btn_num_in[1] = 0;
-	#10 btn_num_in[2] = 1;
+		inactive_in_levels[2] = 1;
+		btn_in[1] = 0;
+	#10 btn_in[2] = 1;
+	inactive_in_levels[1] = 1;
 	#10 btn_down_out[7] = 0;
-		btn_num_in[2] = 0;
+		btn_in[2] = 0;
 	#10 reset = 0;
 	#10 reset = 1;
 	#10 btn_down_out[6] = 1;
