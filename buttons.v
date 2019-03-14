1c9fe4e9499bef383e44c0989d8ad6d327288846
Mateusz Dyrdół <mateuszdyrdol@gmail.com>
2019-03-14 20:12:23 +0100

    buttons updated, final version

--- buttons.v
+++ buttons.v
@@ -5,27 +5,25 @@ parameter BUTTONS_WIDTH = 8
 
 )
 (
-	input clk,
-	input reset,
-	input [BUTTONS_WIDTH-1:0] btn_in, // wewnatrz windy
-	input [BUTTONS_WIDTH-1:0] btn_up_out, //na zewnatrz do gory
-	input [BUTTONS_WIDTH-1:0] btn_down_out,//na zewnatrz na dół
-	input [BUTTONS_WIDTH-1:0] inactivate_in_levels, 
-	input [BUTTONS_WIDTH-1:0] inactivate_out_up_levels,
-	input [BUTTONS_WIDTH-1:0] inactivate_out_down_levels,
-	output reg [BUTTONS_WIDTH-1:0] active_in_levels,
-    output reg [BUTTONS_WIDTH-1:0] active_out_up_levels,
-	output reg [BUTTONS_WIDTH-1:0] active_out_down_levels
-	
+	input							reset,
+	input 		[BUTTONS_WIDTH-1:0] btn_in, // wewnatrz windy
+	input 		[BUTTONS_WIDTH-1:0] btn_up_out, //na zewnatrz do gory
+	input 		[BUTTONS_WIDTH-1:0] btn_down_out,//na zewnatrz na dół
+	input 		[BUTTONS_WIDTH-1:0] inactivate_in_levels, 
+	input 		[BUTTONS_WIDTH-1:0] inactivate_out_up_levels,
+	input 		[BUTTONS_WIDTH-1:0] inactivate_out_down_levels,
+	output reg 	[BUTTONS_WIDTH-1:0] active_in_levels,
+    output reg 	[BUTTONS_WIDTH-1:0] active_out_up_levels,
+	output reg 	[BUTTONS_WIDTH-1:0] active_out_down_levels
 );
-	integer index;
+	reg[3:0] index;
 	
 	always @(*) 
 	begin
 		if(!reset) begin
-			active_in_levels=0;
-			active_out_up_levels=0;
-			active_out_down_levels=0;
+			active_in_levels		=0;
+			active_out_up_levels	=0;
+			active_out_down_levels	=0;
 		end
 		else begin
 			for(index=0; index<BUTTONS_WIDTH; index=index+1) begin
@@ -48,9 +46,9 @@ parameter BUTTONS_WIDTH = 8
 				end else if (inactivate_out_down_levels[index] == 1) begin
 					active_out_down_levels[index] = 0;
 				end
-			end				
+			end
+			
 		end
 	end	
-	
 endmodule 
 //buttons
--- buttons_tb.v
+++ buttons_tb.v
@@ -3,20 +3,18 @@
 module buttons_tb;
 
 	parameter BUTTONS_WIDTH = 8;
-	reg clk;
-	reg reset;
-	reg [BUTTONS_WIDTH-1:0] btn_in; 
-	reg [BUTTONS_WIDTH-1:0] btn_up_out; 
-	reg [BUTTONS_WIDTH-1:0] btn_down_out;
-	reg [BUTTONS_WIDTH-1:0] inactivate_in_levels;
-	reg [BUTTONS_WIDTH-1:0] inactivate_out_up_levels;
-	reg [BUTTONS_WIDTH-1:0] inactivate_out_down_levels;
-	wire [BUTTONS_WIDTH-1:0] active_in_levels;
-	wire [BUTTONS_WIDTH-1:0] active_out_up_levels;
-	wire [BUTTONS_WIDTH-1:0] active_out_down_levels;
+	reg 						reset;
+	reg 	[BUTTONS_WIDTH-1:0] btn_in; 
+	reg 	[BUTTONS_WIDTH-1:0] btn_up_out; 
+	reg 	[BUTTONS_WIDTH-1:0] btn_down_out;
+	reg 	[BUTTONS_WIDTH-1:0] inactivate_in_levels;
+	reg 	[BUTTONS_WIDTH-1:0] inactivate_out_up_levels;
+	reg 	[BUTTONS_WIDTH-1:0] inactivate_out_down_levels;
+	wire	[BUTTONS_WIDTH-1:0] active_in_levels;
+	wire	[BUTTONS_WIDTH-1:0] active_out_up_levels;
+	wire	[BUTTONS_WIDTH-1:0] active_out_down_levels;
 
  buttons buttons_inst(
-		.clk(clk),
 		.reset(reset),
 		.btn_in(btn_in),
 		.btn_up_out(btn_up_out),
@@ -29,16 +27,10 @@ module buttons_tb;
 		.active_out_down_levels(active_out_down_levels)
 		);   
 
-always
-	begin
-		#10 clk=!clk;
-	end	
-		
 initial
 	begin
 	$dumpfile("buttons_tb.vcd");
-	$dumpvars(0,clk,reset,btn_in,btn_up_out,btn_down_out,inactivate_in_levels,inactivate_out_up_levels,inactivate_out_down_levels,active_in_levels,active_out_up_levels,active_out_down_levels);
-	clk = 1'b0;
+	$dumpvars(0,reset,btn_in,btn_up_out,btn_down_out,inactivate_in_levels,inactivate_out_up_levels,inactivate_out_down_levels,active_in_levels,active_out_up_levels,active_out_down_levels);
 	btn_in = 0;
 	btn_up_out = 0;
 	btn_down_out = 0;
@@ -50,19 +42,23 @@ initial
 	#20 btn_up_out[4] = 1;	
 	#10 btn_up_out[5] = 1;
 	#10 btn_in[1] = 1;
+	btn_in[6] = 1;
 	#10 btn_down_out[7] = 1;
 	#10 btn_in[3] = 0;
+	btn_in[1] = 0;
+	btn_in[6] = 0;
 	#10 btn_down_out[7] = 0;
 		btn_up_out[5] = 0;
 	#10 btn_up_out[4] = 0;
 		btn_down_out[7] = 1;
 		inactivate_in_levels[2] = 1;
-		btn_in[1] = 0;
+		inactivate_in_levels[6] = 1;
 	#10 btn_in[2] = 1;
 	inactivate_in_levels[1] = 1;
+	inactivate_in_levels[6] = 0;
 	#10 btn_down_out[7] = 0;
 		btn_in[2] = 0;
-	#10 reset = 0;
+	#40 reset = 0;
 	#10 reset = 1;
 	#10 btn_down_out[6] = 1;
 		btn_up_out[5] = 1;
